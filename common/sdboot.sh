4f409c29b1e8adc79ba4b7641cbd4200247eb5b8
Yuntian Zhang <yt@radxa.com>
2022-08-06 19:09:15 +0800

    Fix efi build
    #FIXME: /etc/machine-id is reset by our first boot setup script. Need to find a way to integrate it with https://www.freedesktop.org/software/systemd/man/machine-id.html#First%20Boot%20Semantics

--- common/sdboot.sh
+++ common/sdboot.sh
@@ -4,7 +4,9 @@ KERNEL=$(find /boot/vmlinuz-*)
 KERNEL=${KERNEL#"/boot/vmlinuz-"}
 MACHINE_ID=$(cat /etc/machine-id)
 
-echo "$(cat /etc/kernel/cmdline) earlyprintk console=tty0 console=ttyAML0,115200n8 console=ttyS2,1500000n8 console=ttyFIQ0,1500000n8" > /etc/kernel/cmdline
+source /etc/defaule/extlinux
+
+echo "$(cat /etc/kernel/cmdline) $APPEND" > /etc/kernel/cmdline
 
 mkdir /boot/efi/$(cat /etc/machine-id)
 
