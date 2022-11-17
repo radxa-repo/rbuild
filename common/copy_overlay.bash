ed4d1216d5ba840a4d6c81fecdedef3de4a900a0
ZHANG Yuntian <yt@radxa.com>
2022-11-17 18:37:59 +0800

    Fix copy_overlay.bash

--- common/copy_overlay.bash
+++ common/copy_overlay.bash
@@ -3,7 +3,7 @@
 set -e
 shopt -s nullglob
 mkdir -p /boot/dtbo
-for i in /usr/lib/linux-image-*/{{ $soc_family }}/overlays/*.dtbo
+for i in /usr/lib/linux-image-*/$1/overlays/*.dtbo
 do
     cp "$i" /boot/dtbo/$(basename "$i").disabled
 done
