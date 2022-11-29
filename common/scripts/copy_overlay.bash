f69041171c61b21309cba61957bba0d995721747
ZHANG Yuntian <yt@radxa.com>
2022-11-29 15:59:34 +0800

    Copy non radxa/overlays as well

--- common/scripts/copy_overlay.bash
+++ common/scripts/copy_overlay.bash
@@ -3,7 +3,7 @@
 set -euo pipefail
 shopt -s nullglob
 mkdir -p /boot/dtbo
-for i in /usr/lib/linux-image-*/$1/overlays/*.dtbo
+for i in /usr/lib/linux-image-*/$1/overlay*/*.dtbo
 do
     cp "$i" /boot/dtbo/$(basename "$i").disabled
 done
