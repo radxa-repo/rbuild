11964f0f19b1c468d9be968f6ec476daec9d4404
ZHANG Yuntian <yt@radxa.com>
2023-03-11 18:48:54 +0800

    Let rsetup handle overlays

--- common/scripts/copy_overlay.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-shopt -s nullglob
-mkdir -p /boot/dtbo
-for i in /usr/lib/linux-image-*/$1/overlay*/*.dtbo
-do
-    cp "$i" /boot/dtbo/$(basename "$i").disabled
-done
